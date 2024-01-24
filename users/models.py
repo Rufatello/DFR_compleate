89177df4c9de446ac168471252462d329734acb1
geydarovr <www.rufat@bk.ru>
2024-01-24 16:44:05 +0300

    4_dz complete

--- users/models.py
+++ users/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 NULLABLE = {'blank': True, 'null': True}
 
+
 class UserRoles(models.TextChoices):
     MEMBER = 'member', _('member')
     MODERATOR = 'moderator', _('moderator')
+
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, max_length=150, verbose_name='Почта')
